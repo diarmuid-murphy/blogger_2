3754e539df1ddc7ab66e261df3ae29d734a1c2ae
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-03-20 16:03:04 -0500

    rails app initiated

---
+++ blogger/config/routes.rb
@@ -0,0 +1,6 @@
+# Rails.application.routes.draw do
+# This is what it used to read, changed due to instruction on this page: http://tutorials.jumpstartlab.com/projects/blogger.html#i0:-up-and-running
+ 
+Blogger::Application.routes.draw do
+  resources :articles
+end
