b60f4376df5f6d1e9a191ff38aa1650bfdb6c935
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-03-20 20:50:28 -0500

    flash added

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -2,5 +2,6 @@
 # This is what it used to read, changed due to instruction on this page: http://tutorials.jumpstartlab.com/projects/blogger.html#i0:-up-and-running
  
 Blogger::Application.routes.draw do
+  root to: 'articles#index'
   resources :articles
 end
